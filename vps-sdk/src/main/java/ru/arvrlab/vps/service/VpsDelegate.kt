0f9578ef50034ec0fccd3d2bd9870df4b14b4750
lastic21 <lastic789@yandex.ru>
2020-12-11 11:58:18 +0300

    добавлен режим докалибрлвки, но пока приходит только fail

--- vps-sdk/src/main/java/ru/arvrlab/vps/service/VpsDelegate.kt
+++ vps-sdk/src/main/java/ru/arvrlab/vps/service/VpsDelegate.kt
@@ -5,6 +5,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.media.Image
+import android.util.Log
 import com.google.ar.core.exceptions.NotYetAvailableException
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
@@ -13,6 +14,7 @@ import com.google.ar.sceneform.math.Vector3
 import kotlinx.coroutines.*
 import okhttp3.MultipartBody
 import ru.arvrlab.vps.extentions.getConvertedCameraStartRotation
+import ru.arvrlab.vps.extentions.toEulerAngles
 import ru.arvrlab.vps.extentions.toMultipartBody
 import ru.arvrlab.vps.extentions.toNewRotationAndPositionPair
 import ru.arvrlab.vps.network.VpsApi
@@ -44,12 +46,16 @@ class VpsDelegate(
     private var cameraAlternativeNode: AnchorNode? = null
     private var rotationNode: AnchorNode? = null
 
+    private var failureCount = 0
+    private var force = true
+    private var firstLocalize = true
+
     private val locationListener: LocationListener = LocationListener { location ->
         lastLocation = location
     }
 
     fun start() {
-        if(isTimerRunning) {
+        if (isTimerRunning) {
             return
         }
 
@@ -75,7 +81,7 @@ class VpsDelegate(
         timerJob = coroutineScope.launch {
             while (true) {
                 delay(settings.timerInterval)
-                if(isActive) {
+                if (isActive) {
                     updateLocalization()
                 }
             }
@@ -110,16 +116,31 @@ class VpsDelegate(
     private suspend fun sendRequest(): Pair<Quaternion, Vector3>? {
         return withContext(Dispatchers.Main) {
             try {
+                Log.i(TAG, "force = $force")
+                Log.i(TAG, "failureCount = $failureCount")
                 val responseDto = VpsApi.getApiService(settings.url).process(
                     getRequestDto(),
                     getMultipart()
                 )
-                //если "done"
-                callback?.onPositionVps(responseDto)
-                responseDto.toNewRotationAndPositionPair()
-            }catch (e: NotYetAvailableException) {
+
+                Log.i(TAG, "status = ${responseDto.responseData?.responseAttributes?.status}")
+                if (responseDto.responseData?.responseAttributes?.status == "done") {
+                    if (!settings.onlyForce) {
+                        force = false
+                    }
+
+                    firstLocalize = false
+                    failureCount = 0
+                    callback?.onPositionVps(responseDto)
+                    responseDto.toNewRotationAndPositionPair()
+                } else {
+                    failureCount++
+                    null
+                }
+
+            } catch (e: NotYetAvailableException) {
                 null
-            }catch (e: CancellationException) {
+            } catch (e: CancellationException) {
                 null
             } catch (e: Exception) {
                 stop()
@@ -131,9 +152,10 @@ class VpsDelegate(
 
     // создать функцию которая сохраняла последнюю битмапу
     private suspend fun getMultipart(): MultipartBody.Part {
-        val image: Image = vpsArFragment.arSceneView.arFrame?.acquireCameraImage() ?: throw NotYetAvailableException(
-            "Failed to acquire camera image"
-        )
+        val image: Image = vpsArFragment.arSceneView.arFrame?.acquireCameraImage()
+            ?: throw NotYetAvailableException(
+                "Failed to acquire camera image"
+            )
         val multipartBody = image.toMultipartBody()
         image.close()
         return multipartBody
@@ -150,7 +172,17 @@ class VpsDelegate(
     private fun createRequestDto(location: Location? = null): RequestDto {
         val request = RequestDto(RequestDataDto())
 
-        request.data.attributes.forcedLocalisation = settings.onlyForce
+        if (!firstLocalize) {
+            if (failureCount >= 2) {
+                force = true
+            }
+        }
+
+        if (!force) {
+            setLocalPos(request)
+        }
+
+        request.data.attributes.forcedLocalisation = force
         request.data.attributes.location.locationId = settings.locationID
 
         location?.run {
@@ -166,8 +198,31 @@ class VpsDelegate(
         return request
     }
 
+    private fun setLocalPos(request: RequestDto) {
+        val anchor =
+            vpsArFragment.arSceneView.session?.createAnchor(vpsArFragment.arSceneView.arFrame?.camera?.pose)
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.setParent(positionNode)
+
+        val localPos = anchorNode.localPosition
+        val localRot = anchorNode.localRotation.toEulerAngles()
+
+        localPos.let {
+            request.data.attributes.location.localPos.run {
+                x = it.x
+                y = it.y
+                z = it.z
+                roll = localRot.z
+                pitch = localRot.x
+                yaw = localRot.y
+            }
+        }
+
+        anchorNode.setParent(null)
+    }
+
     private fun localize(newRotationAndPosition: Pair<Quaternion, Vector3>) {
-        if(!isTimerRunning) {
+        if (!isTimerRunning) {
             return
         }
 
@@ -178,6 +233,11 @@ class VpsDelegate(
 
         cameraAlternativeNode?.worldRotation = getConvertedCameraStartRotation(cameraStartRotation)
         cameraAlternativeNode?.worldPosition = cameraStartPosition
+
+//test
+//        val euler = newRotationAndPosition.first.toEulerAngles()
+//        val newQuart = Quaternion(euler)
+//        rotationNode?.localRotation = newQuart
 
         rotationNode?.localRotation = newRotationAndPosition.first
         positionNode.localPosition = newRotationAndPosition.second
@@ -198,6 +258,8 @@ class VpsDelegate(
         locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
 
     fun stop() {
+        firstLocalize = true
+        force = true
         stopLocalizationUpdate()
         locationManager?.removeUpdates(locationListener)
     }
@@ -236,6 +298,9 @@ class VpsDelegate(
 
     fun enableForceLocalization(enabled: Boolean) {
         settings.onlyForce = enabled
+        if (!enabled) {
+            force = true
+        }
     }
 
     companion object {
